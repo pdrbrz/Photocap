44aee127d5f5740d927f82be0fb30a89ba71b3ac
Pedro Braz <pedroacbraz@gmail.com>
2025-05-09 12:17:53 -0300

    Creating project initial files

---
+++ Photocap/PhotocapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotocapApp.swift
+//  Photocap
+//
+//  Created by Pedro Braz on 09/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotocapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
