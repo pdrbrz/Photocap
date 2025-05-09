8ed711798d9beeffe28a6481777bf8697ed3b79f
Pedro Braz <pedroacbraz@gmail.com>
2025-05-09 17:14:11 -0300

    Adding Localizable and CaptureManager initial logic

--- PhotocapTests/PhotocapTests.swift
+++ PhotocapTests/PhotocapTests.swift
@@ -5,11 +5,10 @@
 //  Created by Pedro Braz on 09/05/25.
 //
 
-import XCTest
 @testable import Photocap
+import XCTest
 
 final class PhotocapTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,9 +27,8 @@ final class PhotocapTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
